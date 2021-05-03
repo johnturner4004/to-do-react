c965f85b972b78478017de9dd49a1ceecb558a8f
johnturner4004 <johnturner4004@gmail.com>
2021-05-03 01:38:23 -0400

    print task name details time added, and due tate on DOM for each task

--- src/components/TaskDetails/TaskDetails.jsx
+++ src/components/TaskDetails/TaskDetails.jsx
@@ -1,11 +1,17 @@
 function TaskDetails(props) {
-  let details = props.details
-  console.log('in TaskDetails', details);
-  return(
+  let details = props.details;
+  console.log("in TaskDetails", details);
+  return (
     <div className="details">
-      <h1>HELLO!!!</h1>
+      <div className="detailsText">
+        <h1>{details.task_name}</h1>
+        <p>{details.description}</p>
+      </div>
+      <div className="time">
+        <p>Time added: {details.time_added} Time due:{details.due_date}</p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default TaskDetails;
